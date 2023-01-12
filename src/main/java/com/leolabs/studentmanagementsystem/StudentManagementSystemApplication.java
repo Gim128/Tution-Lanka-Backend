bf2359026b240bbefaf78dc12362e9506bdc53f1
ican-graphics-01 <lankaican@gmail.com>
2023-01-12 16:34:39 +0530

    initialize new project for tution_Lanka-spring-backend

---
+++ src/main/java/com/leolabs/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leolabs.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+}
